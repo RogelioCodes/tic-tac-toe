7a7fde782b588b226572d2dbefa68d5b1848b52e
RogelioCodes <droozycraft@gmail.com>
2021-03-03 22:21:24 -0800

    reset Game function working, changing turns working, need to add styling

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ const winCombos = [
   [2, 5, 8],
   [0, 4, 8],
 ];
-let message = document.querySelector('[data-winnerMessage]');
+const SQUARES = document.querySelectorAll("[data-square]");
+const winningMessageTextElement = document.querySelector(
+    "[data-winnerMessage]"
+  );
+const squareElements = document.querySelectorAll("[data-square]");
+const myResetButton = document.getElementById("resetButton");
+
 const Player = (sign) => {
   let _sign = sign;
   const getSign = () => _sign;
@@ -18,28 +24,124 @@ const Player = (sign) => {
 
   return { getSign, setSign };
 };
+const displayController = (() => {
+    
+  
 
-//NEED TO UNDERSTAND WHAT IS GETTING INITIALIZED!
-const Gameboard = (() => {
-  let choiceArray = ["", "", "", "", "", "", "", "", ""];
-  let currentPlayer;
-  const playerOne = Player();
-  const playerTwo = Player();
-  playerOne.setSign("x");
-  playerTwo.setSign("o");
-  currentPlayer = playerOne;
-  //make a function to change turn?
-  const pushToArray = function (currentPlayer, playerOne, playerTwo, e) {
-    let index = e;
-    if (currentPlayer === playerOne && choiceArray[index] == "") {
-      choiceArray[index] = playerOne.getSign();
-      changeTurn();
-    } else if (currentPlayer === playerTwo && choiceArray[index] == "") {
-      choiceArray[index] = playerTwo.getSign();
-      changeTurn();
+  //fillInSquare is passed the index of the square we want to mark
+  //this function iterates through the list of user inputs(choiceArray) to find the index of the square that needs to be marked, if the index
+  const fillInSquare = function (squareIndex, currentPlayer) {
+    const squareToBeFilled = document.getElementById(squareIndex);
+
+    for (let i = 0; i < Gameboard.choiceArray.length; i++) {
+      squareToBeFilled.textContent = Gameboard.choiceArray[squareIndex];
+      squareToBeFilled.classList = Gameboard.currentPlayer.getSign();
     }
   };
-  const SQUARES = document.querySelectorAll(".square");
+  function on() {
+    document.getElementById("overlay").style.display = "flex";
+    document.getElementById("overlay").style.flexDirection = "column";
+  }
+
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  return {  on, off, fillInSquare };
+})();
+//The gameboard object model handles most of the logic for the game
+//It calls the 'Player' factory function to create two players, which are represented by X and O
+//
+
+var Gameboard = (() => {
+  const playerOne = Player();
+  const playerTwo = Player();
+  playerOne.setSign("X");
+  playerTwo.setSign("O");
+  var currentPlayer = playerOne;
+
+  
+  choiceArray = ["", "", "", "", "", "", "", "", ""];
+  function init(restart){
+    console.log("restart: " + restart)
+    if(restart == true) { 
+      for(let i = 0 ; i < 9 ; i++){
+      Gameboard.choiceArray.pop()
+    }
+      restart = false ;  
+    }
+    currentPlayer = playerOne;
+   
+    var squareArray = Array.from(SQUARES);
+    squareArray.forEach((square) => {
+      square.classList.remove(playerOne.getSign())
+      square.classList.remove(playerTwo.getSign())
+      square.textContent = "";
+      square.removeEventListener('click', handleClick)
+      square.addEventListener('click', handleClick, { once: true } )
+    })
+    
+    winningMessageTextElement.textContent = null;
+    displayController.off();
+
+      console.log("hello")
+     
+  }
+ 
+
+  function handleClick(e) {
+   
+    const square = e.target;
+    
+    currentPlayer.getSign() == 'O' ? currentPlayer = playerOne : currentPlayer = playerTwo
+    pushToArray(currentPlayer, playerOne, playerTwo,e.target.id);
+    displayController.fillInSquare(e.target.id, currentPlayer);
+    if (checkWin(playerOne) || checkWin(playerTwo) ) {
+          endGame(false, currentPlayer, playerOne, playerTwo);
+        } else if (isDraw(playerOne, playerTwo)) {
+          endGame(true, currentPlayer,playerOne, playerTwo);
+        }
+       
+  }
+
+  //
+ 
+  const pushToArray = function (currentPlayer, playerOne, playerTwo, e) {
+    var index = e;
+    console.log(index);
+    console.log("myCurrentPlayer::::::" + currentPlayer.getSign());
+    if (currentPlayer === playerOne && choiceArray[index] != "X" && choiceArray[index] != "O" ) {
+      console.log("pushing here");
+      choiceArray[index] = playerOne.getSign();
+      changeTurn(currentPlayer, playerOne, playerTwo);
+      console.log("circle? : " + currentPlayer.getSign());
+    } else if (currentPlayer === playerTwo && choiceArray[index] != "X" && choiceArray[index] != "O" ) {
+      console.log("pushing here2");
+      choiceArray[index] = playerTwo.getSign();
+      changeTurn(currentPlayer, playerOne, playerTwo);
+    }
+  }; 
+
+  console.log("starting game");
+  // choiceArray = setChoiceArray();
+  //adds an event listen to each square,
+  //the current player's sign is pushed into the array when they click on an EMPTY square
+
+  
+  function endGame(draw) {
+    if (draw) {
+      winningMessageTextElement.textContent = "Draw!";
+    } else {
+      winningMessageTextElement.textContent = `${
+        currentPlayer == playerOne ? playerOne.getSign() : playerTwo.getSign()
+      }'s win!`;
+    }
+    displayController.on();
+  }
+ 
+
+ 
+  
   function checkWin(currentPlayer) {
     return winCombos.some((combination) => {
       return combination.every((index) => {
@@ -47,56 +149,33 @@ const Gameboard = (() => {
       });
     });
   }
-  const startGame = () => {
-    //adds an event listen to each square,
-    //the current player's sign is pushed into the array when they click on an EMPTY square
-    let squareArray = Array.from(SQUARES);
-    squareArray.forEach((square) => {
-      square.addEventListener("click", (e) => {
-        pushToArray(currentPlayer, playerOne, playerTwo, e.target.id),
-        displayController.fillInSquare(e.target.id);
-        if (checkWin(playerOne) || checkWin(playerTwo)) {
-            if(currentPlayer == playerOne){
-                message.textContent = playerTwo.getSign() + " is a Winner! Thats on god ! ";
-            } 
-            if(currentPlayer == playerTwo){
-                message.textContent = playerOne.getSign() + " is a Winner! Thats on god! ";
-            }
-         displayController.on();
-        } 
-      });
-    });
-  };
-  function endGame(){
-
+  function changeTurn(currentPlayer, playerOne, playerTwo) {
+    console.log("currentPlayer inside changeTurn " + currentPlayer.getSign());
+    console.log("Player1 inside changeTurn " + playerOne.getSign());
+    Gameboard.currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+    console.log("currentPlayer inside changeTurn " + Gameboard.currentPlayer.getSign());
   }
-  const changeTurn = () => {
-    currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
-  };
-  return { currentPlayer, startGame, changeTurn, pushToArray, choiceArray };
-})();
-
-const displayController = (() => {
-//fillInSquare is passed the index of the square we want to mark
-  const fillInSquare = function (index) {
-    let squareIndex = index;
-    for (let i = 0; i < Gameboard.choiceArray.length; i++) {
-      if (squareIndex) {
-        let squareToBeFilled = document.getElementById(squareIndex);
-        squareToBeFilled.textContent = Gameboard.choiceArray[index];
-        squareToBeFilled.classList = Gameboard.choiceArray[index];
-      }
-    }
-  };
-  function on() {
-    document.getElementById("overlay").style.display = "block";
+  function isDraw(playerOne, playerTwo) {
+    //This function checks if the game is a draw by iterating through every square on the gameboard and checking to see
+    //if the square element contains the class "x" or "o"
+    //if every square is marked then it returns True
+    //the squareElement does not have an 'every' method, but we can get around this by destructuring the square elements into an array
+    return [...squareElements].every((square) => {
+      return (
+        square.classList.contains(playerOne.getSign()) ||
+        square.classList.contains(playerTwo.getSign())
+      );
+    });
   }
   
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
- 
-  return { on, off,fillInSquare };
+
+  myResetButton.addEventListener("click", () => {
+    console.log("click"), init(true);
+  });
+  
+  
+  return { choiceArray, init, currentPlayer};
 })();
 
-Gameboard.startGame();
+
+Gameboard.init();
